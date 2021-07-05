1ac71962bad7af9f1033d4199553b0a6620e09c4
Carlos Axel Cruz Bon <acruz@lumston.com>
2021-07-05 17:07:39 -0500

    database name update

--- config/env/development.env.js
+++ config/env/development.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 3000,
-    dbUri: 'mongodb+srv://dbUserAxel:dNR74A7C6q8kU1iP@reworth-cluster.jgsz0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    dbUri: 'mongodb+srv://dbUserAxel:dNR74A7C6q8kU1iP@reworth-cluster.jgsz0.mongodb.net/reworth-db?retryWrites=true&w=majority',
     spotifyApiUrl: 'https://api.spotify.com/v1',
     spotifyTokenUrl: 'https://accounts.spotify.com/api/token',
 }
